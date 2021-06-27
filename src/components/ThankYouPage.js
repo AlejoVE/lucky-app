3f2a388c7730361817da4d5f6516f3c9b84c07e9
Alejandro Montilla <luis.alejandro.499be@gmail.com>
2021-06-27 19:21:14 +0200

    Change thank you message

--- src/components/ThankYouPage.js
+++ src/components/ThankYouPage.js
@@ -7,7 +7,7 @@ export const ThankYouPage = () => {
 		<div className='thanks-container container animate__animated animate__fadeIn'>
 			<Logo />
 			<div className='alert alert-success ' role='alert'>
-				<h2>Votre demande a été enregistrée, merci de participer!</h2>
+				<h2>Your application has been registered, thank you for participating!</h2>
 			</div>
 			<Button path='home' message='Back to Home' buttonClass='warning' />
 		</div>
